070af1056475cbf2cb0ec60059b73b6e68d3a2fb
Nikita Ponomarev <iponomar47@gmail.com>
2018-06-21 00:15:49 +0800

    Fibonachi

--- Fibonachi/Program.cs
+++ Fibonachi/Program.cs
@@ -8,8 +8,24 @@ namespace Fibonachi
 {
     class Program
     {
+        static int Fibonachi(int n)
+        {
+            if (n == 0)
+                return 0;
+            if (n == 1)
+                return 1;
+            else
+                return Fibonachi(n - 1) + Fibonachi(n - 2);
+        }
+
         static void Main(string[] args)
         {
+            int x = Fibonachi(8);
+            Console.WriteLine(x);
+            Console.WriteLine(Fibonachi(4));
+            Console.WriteLine(Fibonachi(6));
+            Console.ReadKey();
         }
+
     }
 }
